26580d631a7330750421193fea8cd0ddb92f45d1
Kataev Evgen <vinegarjack2012@gmail.com>
2019-11-21 01:54:34 +0200

    task5

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 (function (){
     "use strict";
     function recur(n){
-        if (n>1){
-            recur(n-1); 
-        }
-        return console.log(n);
+        
+        if (Math.floor(n/10) === 0 && n%10 !==0){           
+            return console.log(n%10);            
+        }else {
+            console.log(n%10);
+    
+            return recur(Math.floor(n/10));
+        }   
     }
-    recur(10);
+    recur(20008);  
+    //recur(1234);
 })();
